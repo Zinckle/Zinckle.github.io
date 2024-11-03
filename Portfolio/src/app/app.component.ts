b0806b5542663b38af02ebd038463102defc16b2
zinckle <mzinck9@gmail.com>
2024-11-02 21:42:35 -0400

    starting on about section

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { NavigationComponent } from "./navigation/navigation.component";
 import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    MatSlideToggleModule, DashboardComponent, NavigationComponent, AboutComponent],
+    MatSlideToggleModule, DashboardComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
